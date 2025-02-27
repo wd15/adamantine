3166e5f134861b6922958ea2be458989ec8c679f
Daniel Wheeler <daniel.wheeler2@gmail.com>
2025-02-27 13:01:55 -0500

    trying to get trilinos / seacas build working

--- nix/nixpkgs/config.nix
+++ nix/nixpkgs/config.nix
@@ -6,10 +6,21 @@
       trilinos-mpi = prevPkgs.trilinos-mpi.overrideAttrs (final: prev: rec {
         version = "14.4.0";
 
+        buildInputs = prev.buildInputs ++ [ prevPkgs.netcdf prevPkgs.openblas ];
+        
         preConfigure = prev.preConfigure + ''
-          cmakeFlagsArray+=(-DTrilinos_ENABLE_ML=ON);
+          cmakeFlagsArray+=(-DTrilinos_ENABLE_ML=ON \
+                            -DTPL_ENABLE_Matio=OFF \
+                            -DTrilinos_ENABLE_SEACAS=ON \
+                            -DTPL_ENABLE_X11=OFF
+                            -DBLAS_LIBRARY_NAMES="openblas" \
+                            -DBLAS_LIBRARY_DIRS=${prevPkgs.openblas}/lib \
+                            -DLAPACK_LIBRARY_NAMES="openblas" \
+                            -DLAPACK_LIBRARY_DIRS=${prevPkgs.openblas}/lib);
         '';
 
+
+        
         src = prev.src.override {
           rev = "${prev.pname}-release-${prevPkgs.lib.replaceStrings [ "." ] [ "-" ] version}";
           sha256 = "sha256-jbXQYEyf/p9F2I/I7jP+0/6OOcH5ArFlUk6LHn453qY=";
